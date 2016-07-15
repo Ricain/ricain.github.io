4532c81f144294859eff254c1f2b97e69e1e47ca
Jean <jeanmercadier@live.com>
2016-07-15 18:03:18 +0200

    nouveau site: pendule de Newton

---
+++ jean.js
@@ -0,0 +1,99 @@
+jQuery.fn.rotate = function(degrees) {
+    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+                 '-moz-transform' : 'rotate('+ degrees +'deg)',
+                 '-ms-transform' : 'rotate('+ degrees +'deg)',
+                 'transform' : 'rotate('+ degrees +'deg)'});
+    return $(this);
+};
+
+$jean = {
+    angle : 30,
+    time : 3000,
+    level : 0,
+    acc  : 1,
+    colors : ["#2E7D32", "#283593", "#1565C0", "#00695C", "#AD1457", "#c62828"],
+    init : function () {
+        $jean.resize();
+        $(window).resize($jean.resize);
+        $jean.updateColor();
+        $jean.loop();
+    },
+    resize : function () {
+        $(".let_cont .let_fil").css("height", parseInt($(document).height()/3+5) + "px");
+    },
+    updateColor : function () {
+        $col = $jean.colors[Math.floor(Math.random()*$jean.colors.length)];
+        $("body").css("color", $col);
+        $(".links a").css("color", $col);
+    },
+    loop : function () {
+        if ($jean.level == 0) {
+            $jean.j.up();
+            $jean.level += 1;
+            return;
+        }
+        if ($jean.level == 1) {
+            $jean.j.down();
+            $jean.level += 1;
+            return;
+        }
+        if ($jean.level == 2) {
+            $jean.n.up();
+            $jean.level += 1;
+            return;
+        }
+        if ($jean.level == 3) {
+            $jean.n.down();
+            $jean.level = 0;
+            return;
+        }
+    },
+    j : {
+        up : function () {
+            $laps = $jean.time/$jean.angle;
+            $("#j_cont").attr("rot_obj", $jean.angle);
+            $("#j_cont").attr("rot_cur", 0);
+            $jean.accel($("#j_cont"), $laps)
+        },
+        down : function () {
+            $laps = $jean.time/$jean.angle;
+            $("#j_cont").attr("rot_obj", 0);
+            $("#j_cont").attr("rot_cur", $jean.angle);
+            $jean.accel($("#j_cont"), $laps)
+        }
+    },
+    n : {
+        up : function () {
+            $laps = $jean.time/$jean.angle;
+            $("#n_cont").attr("rot_obj", -$jean.angle);
+            $("#n_cont").attr("rot_cur", 0);
+            $jean.accel($("#n_cont"), $laps)
+        },
+        down : function () {
+            $laps = $jean.time/$jean.angle;
+            $("#n_cont").attr("rot_obj", 0);
+            $("#n_cont").attr("rot_cur", -$jean.angle);
+            $jean.accel($("#n_cont"), $laps)
+        }
+    },
+    accel : function ($tag, $laps) {
+        $rec = function () {
+            $obj = parseInt($tag.attr("rot_obj"));
+            $cur = parseInt($tag.attr("rot_cur"));
+            $rot = 0;
+            if      ($obj>$cur) $rot = +1;
+            else if ($obj<$cur) $rot = -1;
+            else    {
+                $jean.loop();
+                return;
+            }
+            $acc = $obj/
+            $tag.rotate($cur+$rot);
+            $tag.attr("rot_cur", $cur+$rot);
+            setTimeout($rec, $laps/Math.abs($obj-$cur+$jean.acc));
+        }
+        $rec();
+    }
+}
+
+$(document).ready($jean.init);
